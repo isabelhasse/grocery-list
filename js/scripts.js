ccddedc13b7e048b8c8573a07498d86ed2795166
Isabel Hasse and John Remboldt <john@>
2017-11-01 19:08:43 -0700

    add js to create array of form responses.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+var groceryList = [];
+
 $(document).ready(function(){
   $("form#groceries").submit(function(event){
-    
+    groceryList.push($("#item-1").val());
+    groceryList.push($("#item-2").val());
+    groceryList.push($("#item-3").val());
+    groceryList.push($("#item-4").val());
+    console.log(groceryList);
+
+    event.preventDefault();
   });
 });
